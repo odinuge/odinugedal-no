9baf107b1167ccf9e55fe836f3541939d6b5c142
Odin Ugedal <odin@ugedal.com>
2020-02-13 21:27:28 +0100

    Bump design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import Link from "next/link";
 import Head from "next/head";
-import Nav from "../components/nav";
 
 const Index = () => (
   <div>
@@ -13,86 +11,30 @@ const Index = () => (
       />
     </Head>
 
-    <Nav />
-
-    <div className="hero">
-      <h1 className="title">Odin Ugedal</h1>
-      <p className="description">
-        Student. MSc in computer science, NTNU
-        <br />
-        Doing mostly systems programming & cloud Native
-      </p>
-
-      <div className="row">
-        <Link href="https://github.com/odinuge">
-          <a className="card">
-            <h3>GitHub</h3>
-            <p>I do most of my stuff on github</p>
-          </a>
-        </Link>
-        <Link href="https://www.linkedin.com/in/odinugedal/">
-          <a className="card">
-            <h3>Linkedin</h3>
-            <p>You know the drill</p>
-          </a>
-        </Link>
-        <Link href="mailto:odin@ugedal.com">
-          <a className="card">
-            <h3>Email</h3>
-            <p>odin@ugedal.com</p>
-          </a>
-        </Link>
-      </div>
-    </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
+    <pre>
+      Odin Ugedal
+      <br />
+      <br />
+      Student. MSc in computer science, NTNU Doing mostly systems programming &
+      Cloud Native
+      <br />
+      <br />
+      GitHub:{"   "}
+      <a href="https://github.com/odinuge">https://github.com/odinuge</a>
+      <br />
+      LinkedIn:{" "}
+      <a href="https://www.linkedin.com/in/odinugedal/">
+        https://www.linkedin.com/in/odinugedal/
+      </a>
+      <br />
+      Email:{"    "}
+      <a href="mailto:odin@uged.al">odin@ugedal.com</a>
+      <br />
+      Keybase:{"  "}
+      <a href="https://keybase.io/odinugedal">https://keybase.io/odinugedal</a>
+    </pre>
   </div>
 );
 
-Index.getInitialProps = async () => ({});
+Index.getInitialProps = async () => ({ foo: "bar" });
 export default Index;
